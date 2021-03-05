e71f0352478c745cbfbe2c734423001c471eee0f
cjh1249131356 <cjh1249131356@gmail.com>
2021-03-05 14:11:23 +0800

    sdm845-common: Build Mock Powerstats HAL
    Signed-off-by: krtonia <krtonia@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -356,7 +356,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power.stats@1.0-service.xiaomi
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
