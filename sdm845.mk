1aa9c31d9cf773c6db2867b7ce73d65de865454e
krtonia <krtonia@gmail.com>
2022-10-06 23:09:48 +0530

    sdm845-common: Nuke xiaomi xiaomi-libperfmgr

--- sdm845.mk
+++ sdm845.mk
@@ -330,7 +330,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
